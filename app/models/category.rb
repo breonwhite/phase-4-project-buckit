5688021c1d0e5eb7a5e540f2606e7eee4ea992ce
Breon White <bdwhite92@gmail.com>
2023-01-16 18:59:04 -0500

    Add models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
-class Category < ApplicationRecord
+class Category < ActiveRecord::Base
+    # belongs_to :items
+    # belongs_to :users, -> { distinct }, through: items
+    # has_many :items
+    
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
-class Item < ApplicationRecord
+class Item < ActiveRecord::Base
+    belongs_to :user
+    has_many :categories
 end
