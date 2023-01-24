fbee3dab142cfece121a3c19b1c7b818b2d39f32
Breon White <bdwhite92@gmail.com>
2023-01-24 18:28:45 -0500

    Add routes for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
+
+    def index
+        user = User.all
+        render json: user, include: :items
+    end
+
+    def show
+        user = User.find(params[:id])
+        if user
+            render json: user
+        else
+            render json: "Not found"
+        end
+    end
+
+    def create
+        user = User.create(user_params)
+        if user
+            render json: user
+        else
+            render json: "Not found"
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :first_name, :last_name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  resources :items, only: [:index, :show, :create]
+  resources :categories, only: [:index]
+  resources :users, only: [:index, :show, :create]
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  
+  
+  # get 'items', to: 'items#index'
+  # get '/items/:id', to: 'items#show'
+
 end
