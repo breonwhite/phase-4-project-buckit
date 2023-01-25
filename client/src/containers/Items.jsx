a21909f95dfbdc47adce23b094c0b8d80740d8d6
Breon White <bdwhite92@gmail.com>
2023-01-24 22:49:20 -0500

    Add item edit button

--- client/src/containers/Items.jsx
+++ client/src/containers/Items.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Items = () => {
   const [ items, setItems ] = useState([])
@@ -17,7 +19,13 @@ const Items = () => {
     <div>
         <h1>All Items</h1>
         <ul>
-          {items.map((item, index) => <li key={ index }>{ item.category.title } : { item.description }</li>)}
+          {items.map((item, index) => <li key={ index }>{ item.category.title } : { item.description } 
+           <Link to={`/items/${item.id}`}>
+            <button type="button">
+                Edit
+            </button>
+          </Link>
+          </li>)}
         </ul>
     </div>
   )
