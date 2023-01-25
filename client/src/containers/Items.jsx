a7b3e5aa5c0de19e3764f2e4f080762cbb4f41ef
Breon White <bdwhite92@gmail.com>
2023-01-24 22:13:52 -0500

    List all items on Items page

--- client/src/containers/Items.jsx
+++ client/src/containers/Items.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Items = () => {
-  return (
+  const [ items, setItems ] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/items")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        setItems(data)
+    })
+  }, [])
+
+  
+    return (
     <div>
         <h1>All Items</h1>
+        <ul>
+          {items.map((item, index) => <li key={ index }>{ item.category.title } : { item.description }</li>)}
+        </ul>
     </div>
   )
 }
