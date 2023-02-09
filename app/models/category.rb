b38e95dba77c0aeb931c8116f7cbdf88f8022dcd
Breon White <bdwhite92@gmail.com>
2023-02-09 00:52:35 -0500

    Final commits to resolve deploy issue

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
-    # belongs_to :items
-    # belongs_to :users, -> { distinct }, through: items
+    has_and_belongs_to_many :items
+    has_and_belongs_to_many :users, :through => :items
     # has_many :items
     
 end
