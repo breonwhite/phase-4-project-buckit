3de3e42bc3b5d61cb2d9af0e787b2f42f3339c5a
Breon White <bdwhite92@gmail.com>
2023-02-08 22:36:59 -0500

    Get update feature working, and cleaned up code

--- client/src/containers/item/Item.jsx
+++ client/src/containers/item/Item.jsx
@@ -1,33 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect, useContext } from 'react'
+import { UserContext } from '../../context/user' 
+import { useParams, useLocation } from 'react-router-dom'
 import ItemEdit from './ItemEdit'
-import { useNavigate } from 'react-router-dom'
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import paper from '../../images/paper.jpg';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import UnathorizedPage from '../../components/UnathorizedPage';
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent';
 
 const Item = () => {
-    const [ item, setItem ] = useState({})
-    const [ loading, setLoading ] = useState(true)
-    const {id} = useParams()
-    //When a user clicks the edit button, it displays the edit form
-    const [ form, setForm ] = useState(false)
-    const navigate = useNavigate();
+    const { loggedIn } = useContext(UserContext); // Data captured from context in User.jsx
+    
+    const location = useLocation(); // Captures the state of the item from ItemCard.jsx, pushes it through the link
+    
+    const [ item, setItem ] = useState(location.state?.item)
+    const [ form, setForm ] = useState(false) // When a user clicks the edit button, it displays the edit form
 
+    const {id} = useParams() // Captures id from item in the params
+    
     useEffect(() => {
         fetch(`/items/${id}`)
         .then(response => response.json())
         .then(data => {
+            console.log(`Page Reloaded: ${data}`)
             setItem(data)
-            setLoading(false)
         })
     }, [])
-
-    const editItem = (thisItem) => {
-        console.log(thisItem)
+    
+    const editItem = (item) => {
         //only permits the description and the category of the item to be updated
         const updatedItem = {
-            description: thisItem.description,
-            category_id: thisItem.category_id,
+            description: item.description,
+            category_id: item.category_id,
         }
-        fetch(`/items/${thisItem.id}`, {
+        fetch(`/items/${item.id}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type' : 'application/json',
@@ -37,28 +47,56 @@ const Item = () => {
         })
         .then(response => response.json())
         .then(data => {
+            console.log(data)
             setItem(data)
             setForm(false)
         })
     }
-
-    if(loading) {
-        return <h1>Loading Item Details</h1>
-    } else {
+    
+    if(loggedIn) {
     return (
-    <div>
-        <h1>Item Details</h1><br />
-        [BACK BUTTON] - [DELETE BUTTON HERE]
-        <br/><br/>
-        { item.description }<br/>
-        { item.category.title } <br/>
-        {/* Displays a form when button is clicked */}
-        { form ? 
-            <ItemEdit thisItem={item} editItem={editItem} /> : 
-            <button onClick={ () => {setForm(true)} }>Edit Item</button>
-        }
-    </div>
+    <Box
+        style={{
+        backgroundImage: `url(${paper})`,
+        backgroundSize: "cover",
+        height: "100%",
+        color: "#f5f5f5"
+        }}>  
+        <Grid 
+          container
+          direction="column"
+          justify="center"
+          spacing={2}
+          style={{ minHeight: '100vh', width: '90%', margin: 'auto', padding: '10px' }}
+          >
+            <Grid item xs={3}>
+                <Button variant="contained" href="/items/" startIcon={<ArrowLeftIcon />}>
+                    BACK TO ITEMS
+                </Button><br />
+                <Typography variant="overline" gutterBottom sx={{color: '#212121' }}>
+                    Item Details
+                </Typography>
+                <Card sx={{ width: '95%', padding: '10px', backgroundColor: "transparent" }} elevation={24}>
+                    <CardContent>
+                        <Typography variant="overline" gutterBottom sx={{color: '#212121' }}>
+                            Category: { item.category.title }
+                        </Typography><br />
+                        <Typography variant="h3" gutterBottom sx={{color: '#212121' }}>
+                            { item.description }
+                        </Typography>
+                    </CardContent>
+                    { form ? 
+                        <ItemEdit thisItem={item} editItem={editItem} /> : 
+                        <Button fullWidth variant="contained" size="medium" onClick={ () => {setForm(true)} }>Edit Item</Button>
+                    }
+                </Card>
+            </Grid>
+        </Grid>
+    </Box>
+    )
+    } else return (
+    <UnathorizedPage />
   )
-}}
+}
 
 export default Item
